df4f5f0a0e1bd4c25f9465ccd969069edbdcaa4d
asakura <asakura@asakura-no-MacBook-Air.local>
2015-06-11 00:43:34 +0900

    ゲームのシーン切り替えぐらいまで作った

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,37 +1,144 @@
 "use strict";
 
-var _get = function get(_x, _x2, _x3) { var _again = true; _function: while (_again) { var object = _x, property = _x2, receiver = _x3; desc = parent = getter = undefined; _again = false; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { _x = parent; _x2 = property; _x3 = receiver; _again = true; continue _function; } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } } };
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; }
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-function dd(val) {
-  console.log(val);
-}
+$(function () {
+	// すべてのビューモデルを格納するビューモデル
+	var root_vm = {};
+	// テトリスの盤面のマスの数
+	var ROW_NUM = 20;
+	var COL_NUM = 10;
+	// ゲームの状態を示す定数
+	var START = 1;
+	var PLAYING = 2;
+	var PAUSE = 3;
 
-var Test = function Test() {
-  _classCallCheck(this, Test);
+	function dd(message) {
+		console.log(message);
+	}
 
-  dd("Test");
-};
+	var GameViewModel = (function () {
+		function GameViewModel() {
+			_classCallCheck(this, GameViewModel);
 
-var SubTest = (function (_Test) {
-  function SubTest() {
-    _classCallCheck(this, SubTest);
+			this.mode = ko.observable(START);
+			this.attach_events();
+			this.stage = new Stage();
+		}
 
-    _get(Object.getPrototypeOf(SubTest.prototype), "constructor", this).call(this);
-    dd("SubTest");
-  }
+		_createClass(GameViewModel, [{
+			key: "attach_events",
+			value: function attach_events() {
+				$("html").keydown(function (e) {
+					if (root_vm["game_vm"].mode() == PLAYING) {
+						switch (e.which) {
+							case 39:
+								// right
+								dd("right");
+								break;
+							case 37:
+								// left
+								dd("left");
+								break;
+							case 38:
+								// up
+								dd("up");
+								break;
+							case 40:
+								// down
+								dd("down");
+								break;
+						}
+					}
+				});
+			}
+		}, {
+			key: "start",
+			value: function start() {
+				// thisがroot_vmになってしまうため
+				// this.method()ではなくroot_vm['game_vm'].method()としている
+				root_vm["game_vm"].mode(PLAYING);
+				root_vm["game_vm"].stage.start();
+			}
+		}, {
+			key: "reset",
+			value: function reset() {
+				root_vm["game_vm"].mode(START);
+				root_vm["game_vm"].stage.pause();
+			}
+		}, {
+			key: "pause",
+			value: function pause() {
+				root_vm["game_vm"].mode(PAUSE);
+				root_vm["game_vm"].stage.pause();
+			}
+		}]);
 
-  _inherits(SubTest, _Test);
+		return GameViewModel;
+	})();
 
-  return SubTest;
-})(Test);
+	var Stage = (function () {
+		function Stage() {
+			_classCallCheck(this, Stage);
 
-var test = new SubTest();
+			this.intervalId = 0;
+		}
 
-var obj = {
-  __proto__: test
-};
-dd(obj);
+		_createClass(Stage, [{
+			key: "start",
+			value: function start() {
+				this.intervalId = setInterval(this.frame, 2000);
+			}
+		}, {
+			key: "pause",
+			value: function pause() {
+				clearInterval(this.intervalId);
+			}
+		}, {
+			key: "frame",
+			value: function frame() {
+				console.log("frame");
+			}
+		}]);
+
+		return Stage;
+	})();
+
+	var Square = function Square(value) {
+		var _this = this;
+
+		_classCallCheck(this, Square);
+
+		this.value = ko.observable(value);
+		this.text = ko.computed(function () {
+			switch (_this.value()) {
+				case 0:
+					return "";
+				case 1:
+					return "actived";
+				case 2:
+					return "freezed";
+			}
+		});
+	};
+
+	var TileViewModel = function TileViewModel() {
+		_classCallCheck(this, TileViewModel);
+
+		var i;
+		var j;
+		this.square = [];
+		for (i = 0; i < ROW_NUM; i++) {
+			this.square[i] = [];
+			for (j = 0; j < COL_NUM; j++) {
+				this.square[i][j] = new Square(0);
+			}
+		}
+	};
+
+	root_vm["game_vm"] = new GameViewModel();
+	root_vm["tile_vm"] = new TileViewModel();
+	ko.applyBindings(root_vm);
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,165 @@
-(function(){
-  
-})();
+$(function(){
+	// すべてのビューモデルを格納するビューモデル
+	var root_vm = {};
+	// テトリスの盤面のマスの数
+	const ROW_NUM = 20;
+	const COL_NUM = 10;
+	// ゲームの状態を示す定数
+	const START = 1;
+	const PLAYING = 2;
+	const PAUSE = 3;
+	// マスの状態を示す定数
+	const EMPTY = 0;
+	const ACTIVED = 1;
+	const FREEZED = 2;
+	// デバッグ用
+	function dd(message){
+		console.log(message);
+	}
+	
+	class GameViewModel{
+		constructor(){
+			this.mode = ko.observable(START);
+			this.attach_events();
+			this.stage = new Stage();
+		}
+		attach_events(){
+			$('html').keydown(function(e){
+				if(root_vm["game_vm"].mode() == PLAYING){
+					switch(e.which){
+					case 39: // right
+						dd("right");
+						break;
+					case 37: // left
+						dd("left");
+						break;
+					case 38: // up
+						dd("up");
+						break;
+					case 40: // down
+						dd("down");
+						break;
+					}
+				}
+			});
+		}
+		start(){
+			// knockoutのビューから呼ばれた時、thisがroot_vmになってしまうため
+			// this.method()ではなくroot_vm['game_vm'].method()としている
+			root_vm['game_vm'].mode(PLAYING);
+			root_vm['game_vm'].stage.start();
+		}
+		reset(){
+			root_vm['game_vm'].mode(START);
+			root_vm['game_vm'].stage.pause();
+		}
+		pause(){
+			root_vm['game_vm'].mode(PAUSE);
+			root_vm['game_vm'].stage.pause();
+		}
+	}
+	class Stage{
+		constructor(){
+			this.intervalId = 0;
+		}
+		start(){
+			this.intervalId = setInterval(this.frame, 2000);
+		}
+		pause(){
+			clearInterval(this.intervalId);
+		}
+		frame(){
+			console.log("frame");
+		}
+	}
+	class Block{
+		constructor(){
+		}
+	}
+	// *
+	// *
+	// *
+	// *
+	class IBlock extends Block{
+		constructor(){
+			super();
+		}
+	}
+	// *
+	// *
+	// * *
+	class LBlock extends Block{
+		constructor(){
+			super();
+		}
+	}
+	//   *
+	//   *
+	// * *
+	class JBlock extends Block{
+		constructor(){
+			super();
+		}
+	}
+	// *
+	// * *
+	// *
+	class TBlock extends Block{
+		constructor(){
+			super();
+		}
+	}
+	// * *
+	// * *
+	class OBlock extends Block{
+		constructor(){
+			super();
+		}
+	}
+	// * *
+	//   * *
+	class ZBlock extends Block{
+		constructor(){
+			super();
+		}
+	}
+	//   * *
+	// * *
+	class SBlock extends Block{
+		constructor(){
+			super();
+		}
+	}
+	
+	class Square{
+		constructor(value){
+			this.value = ko.observable(value);
+			this.text = ko.computed(()=>{
+				switch(this.value()){
+				case EMPTY:
+					return '';
+				case ACTIVED:
+					return 'actived';
+				case FREEZED:
+					return 'freezed';
+				}
+			});
+		}
+	}
+	class TileViewModel{
+		constructor(){
+			var i;
+			var j;
+			this.square = [];
+			for(i = 0; i < ROW_NUM; i++){
+				this.square[i] = [];
+				for(j = 0; j < COL_NUM; j++){
+					this.square[i][j] = new Square(0);
+				}
+			}
+		}
+	}
+	root_vm['game_vm'] = new GameViewModel();
+	root_vm["tile_vm"] = new TileViewModel();
+	ko.applyBindings(root_vm);
+});
