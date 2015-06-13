e212b38c37d7fd50f634634ba9359c3cbd88cdbf
asakura <asakuradev@gmail.com>
2015-06-13 17:47:59 +0900

    Symbolに対応していないブラウザで動かない問題に対応

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var babel = require('gulp-babel');
-
+var source = require('vinyl-source-stream');
+var browserify = require("browserify");
+var babelify = require('babelify');
 gulp.task('sass', function(){
 	return gulp
 		.src('./src/scss/*.scss')
 		.pipe(sass())
 		.pipe(gulp.dest('./dist/css'));
 });
-
-gulp.task('babel', function() {
-	return gulp
-	    .src('./src/js/*.js')
-	    .pipe(babel())
-	    .pipe(gulp.dest('./dist/js'));
+gulp.task("ES6toES5",function(){
+	return browserify("./src/js/main.js", { debug: true })
+	    .transform(babelify.configure({
+			optional: ["runtime"] 
+		})
+				  )
+	    .bundle()
+	    .pipe(source("main.js"))
+	    .pipe(gulp.dest("./dist/js"));
 });
 
 gulp.task('watch',function (){
 	gulp.watch('./src/scss/*.scss',['sass']);
-	gulp.watch('./src/js/*js',['babel']);
+	gulp.watch('./src/js/*js',['ES6toES5']);
 });
 
-gulp.task('default', ['sass','babel']);
+gulp.task('default', ['sass','ES6toES5']);
